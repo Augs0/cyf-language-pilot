a7dd15320471fe45cfc39de4413c4bceeac6598e
August <hachigatsu14@outlook.com>
2021-01-05 11:46:22 +0000

    gulp tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
 var render = require('gulp-nunjucks-render');
+var cssPath = 'src/**/*.css';
+var njkPath = 'views/*.+(njk)';
 
+gulp.task('css', function(){
+    return gulp.src(cssPath)
+    .pipe(gulp.dest('public'))
+})
 
 gulp.task('nunjucks', function() {
-  return gulp.src('views/*.+(njk)')
+  return gulp.src(njkPath)
   .pipe(render({
       path: ['views']
     }))
   .pipe(gulp.dest('public'))
 });
+
+exports.default = gulp.series(
+    'css',
+    'nunjucks'
+);
+// gulp.task('default', ['css', 'nunjucks'], function() {
+// });
